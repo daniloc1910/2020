3bb3ae9753413666708c4633258947e28df31dcb
Danilo Campos <daniloblk@gmail.com>
2020-04-19 11:43:31 +0200

    1142

--- pages/museu-nacional-da-republica.vue
+++ pages/museu-nacional-da-republica.vue
@@ -3,7 +3,7 @@
     <HeaderInterna
       title="MUSEU NACIONAL DA REPÚBLICA"
       year="2019"
-      role="Design, development"
+      role="Design, Development"
       at='Twoo<span class="r">®</span>'
       description='Website design and development for Museu Nacional de República, located in Brasília and designed by the architect Oscar Niemeyer.<br><br>Project: manufatura.org<br>Creative Direction and Art Direction: Marcos Mendes Manente<br>Brand Strategy: Elisa Mendes<br>Research: Antônio Bonifácio and Paulo Henrique Paranhos<br>Copywriter: Bruna Amora<br>Graphic Design: Leo Porto and Felipe Rocha<br>Design Assistant: Vinicius Caetano<br>Motion: Lais Ikoma and Ronaldo Arthur Vidal<br>Web Design and Development: Carlos Kun and Danilo Campos<br>Content and Social Media: Front Filmes<br><br><a target="_blank" href="http://garage.danilocampos.com.br">See website</a>'
     />
@@ -28,15 +28,13 @@ import HeaderInterna from '~/components/HeaderInterna.vue'
 import Img100 from '~/components/Img100.vue'
 import Img50Right from '~/components/Img50Right.vue'
 import Video50Left from '~/components/Video50Left.vue'
-import ProjectListItem from '~/components/ProjectListItem.vue'
 export default {
   layout: 'interna',
   components: {
     HeaderInterna,
     Img100,
     Img50Right,
-    Video50Left,
-    ProjectListItem
+    Video50Left
   },
 
   data() {
